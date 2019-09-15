439691e19328b9ca9fe4ae74aebc27a98e258efa
limeng <seemoonup@gmail.com>
2019-09-15 18:35:15 +0800

    bit-admin

--- src/components/Upload/UploadFile.vue
+++ src/components/Upload/UploadFile.vue
@@ -19,9 +19,9 @@
 import { getToken } from '@/api/qiniu'
 export default {
   name: 'UploadFile',
-  actionPath: 'https://upload-z1.qiniup.com',
   data() {
     return {
+      actionPath: 'https://upload-z1.qiniup.com',
       dataObj: { token: '', key: '' },
       tempUrl: '',
       fileList: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }]
